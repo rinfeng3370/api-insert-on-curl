bf0ef83395ffbc1a53dfa4ee6e22d855c2939a39
rinfeng3370 <52902231+rinfeng3370@users.noreply.github.com>
2019-11-15 16:46:33 +0800

    queue upload

---
+++ Jobs/QueueJob.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use DB;
+
+class QueueJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    protected $item;
+
+    public function __construct($item)
+    {
+        //
+        $this->item = $item;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        DB::table('queues')->insert($this->item);
+    }
+}
